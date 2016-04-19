e3ad181a161575ebae6359f442b614d8908a4aaf
vp4655 <tino.100@hotmail.com>
2016-04-20 00:09:27 +0200

    Project initialized

---
+++ WebApplication4/WebApplication4/Angular/controllers/uploadController.js
@@ -0,0 +1,17 @@
+﻿'use strict';
+
+angular.module('angularUploadApp')
+    .controller('fileUploadCtrl', function ($scope, $http, $timeout, FileUploader) {
+        $scope.upload = [];
+        $scope.UploadedFiles = [];
+
+        var uploader = $scope.uploader = new FileUploader({
+            url: './api/upload'
+        });
+
+        uploader.onSuccessItem = function (fileItem, response, status, headers) {
+            console.info('onSuccessItem', fileItem, response, status, headers);
+            $scope.UploadedFiles.push(response);
+        };
+
+    });
---
+++ WebApplication4/WebApplication4/Infrastructure/UploadMultipartFormProvider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication4.Infrastructure
+{
+    public class UploadMultipartFormProvider : MultipartFormDataStreamProvider
+    {
+        public UploadMultipartFormProvider(string rootPath) : base(rootPath) { }
+
+        public override string GetLocalFileName(HttpContentHeaders headers)
+        {
+            if (headers != null &&
+                headers.ContentDisposition != null)
+            {
+                return headers
+                    .ContentDisposition
+                    .FileName.TrimEnd('"').TrimStart('"');
+            }
+
+            return base.GetLocalFileName(headers);
+        }
+    }
+}
